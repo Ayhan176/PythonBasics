f0ec4a982b05c0766241c38f8a1569b92ca0e94c
ayhanyazici <ayhanyazici@unitybox.de>
2019-12-12 23:05:17 +0100

    Homework mood

---
+++ mood.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood today? ")
+
+if mood == "happy":
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "excited":
+    print("Yeehaw!")
+elif mood == "relaxed":
+    print("It's a good day to chill!")
+else:
+    print("I don't recognize this mood.")
